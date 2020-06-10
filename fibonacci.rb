323541270052d0ab54ed277c8b259cd3ed6853f0
Dylan Williams <happyfrog578+github@gmail.com>
2020-06-10 20:58:59 +1000

    small code cleanup

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,11 +1,9 @@
 def fib_iterate(num)
   num1 = 0
   num2 = 1
-  puts num1
-  puts num2
-  (num - 1).times do
+  num.times do
+    puts num1
     num3 = num1 + num2
-    puts num3
     num1 = num2
     num2 = num3
   end
@@ -13,9 +11,9 @@ end
 
 def fib_recur(num, int1 = 0, int2 = 1)
   puts int1
-  num.zero? ? return : fib_recur(num - 1, int2, int1 + int2)
+  num == 1 ? return : fib_recur(num - 1, int2, int1 + int2)
 end
 
-fib_iterate(30)
-fib_recur(30)
+fib_iterate(5)
+fib_recur(5)
 
