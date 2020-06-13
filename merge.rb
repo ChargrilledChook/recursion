5d6a21a30e4976429cc14506061c4e56d66827a4
Dylan Williams <happyfrog578+github@gmail.com>
2020-06-13 10:44:06 +1000

    add malans pseudocode

---
+++ merge.rb
@@ -0,0 +1,15 @@
+def merge_sort(array)
+
+end
+
+# on in input of n elements
+# if n < 2
+  # return
+# else
+  # sort left half of elements
+  # sort right half of elements
+  # merge sorted halves
+
+
+
+
